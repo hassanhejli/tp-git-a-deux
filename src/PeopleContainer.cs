e5118e4d500117480437a65e04035b00f08a58e4
CrochetFlorent <crochet.florent@gmail.com>
2024-01-23 15:06:42 +0100

    tp 05

--- src/PeopleContainer.cs
+++ src/PeopleContainer.cs
@@ -1,11 +1,15 @@
-public List SortByLastName(List _noms)
-{
-    _noms.Sort((x,y) => string.Compare(x.nom,y.nom));
-    return _noms;
-}
+public class{
 
-public List SortByFirstName(List _prenoms)
-{
-    _prenoms.Sort((x,y) => string.Compare(x.prenom,y.prenom));
-    return _prenoms;
+        public List SortByLastName(List _personnes)
+        {
+            _personnes.nom.Sort((x,y) => string.Compare(x.nom,y.nom));
+            return _noms;
+        }
+
+        public List SortByFirstName(List _personnes)
+        {
+            _personnes.prenom.Sort((x,y) => string.Compare(x.prenom,y.prenom));
+            return _prenoms;
+        }
+
 }
