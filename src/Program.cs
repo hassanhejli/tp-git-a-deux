eed37d2693883331af96e0670916bebe3737341d
AurelienChem <AurelienChem@users.noreply.github.com>
2024-01-23 14:36:34 +0100

    tp 3

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string nom;
+string prenom;
+
+Console.WriteLine("Saisir le nom");
+nom = Console.ReadLine();
+
+Console.WriteLine("Saisir le prénom");
+prenom = Console.ReadLine();
+
+Console.WriteLine("Le nom est " + nom + " le prenom est " + prenom);
