2108312d29576be52f519836bf42534e54386134
CrochetFlorent <crochet.florent@gmail.com>
2024-01-24 16:11:08 +0100

    Commit Procédures/Fonctions

--- TP01/Person.cs
+++ TP01/Person.cs
@@ -3,31 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP01;
 
 namespace TP01
 {
     public class Person
     {
-        public string nom {get;set; }
-        public string prenom {get; set; }
+        public string nom {get;set;}
+        public string prenom {get; set;}
 
         //Constructeur avec paramètres
         public Person(string _nom, string _prenom)
         {
             this.nom = _nom;
             this.prenom = _prenom;
-        }
-
-        //Getter du nom
-        public string getNom()
-        {
-            return this.nom;
-        }
-
-        //Getter du prénom
-        public string getPrenom()
-        {
-            return this.prenom;
         }
 
         //Fonction de comparaison de deux personnes
@@ -40,6 +29,28 @@ namespace TP01
             return false;
         }
 
+        public void remplissage(List<Person> listePersonnes, List<String> noms, List<String> prenoms,Person myPerson)
+        {
+            //Booleen visant à tester si une personne est présente dans la liste
+            bool testPersonne = false;
+
+            //Test pour toute la liste de personnes si la nouvelle personne est présente dedans
+            for (int i = 0; i < listePersonnes.Count && !testPersonne; i++)
+            {
+                //On compare(grace à la méthode de la classe Person) la personne de la liste à l'indice i avec la personne créée plus haut
+                if (listePersonnes[i].comparer(myPerson))
+                {
+                    testPersonne = true;
+                }
+            }
+            //Si la personne n'est pas présente, on l'ajoute
+            if (testPersonne == false)
+            {
+                listePersonnes.Add(myPerson);
+                noms.Add(myPerson.nom);
+                prenoms.Add(myPerson.prenom);
+            }
+        }         
 
     }
 }
